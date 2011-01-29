bd44b76b0b62d14bc40741339f0b1c1a1399ae12
Thomas Wisniewski <thomas@gazaro.com>
2011-01-29 13:58:17 -0500

    Added initial category-filter code and UI, including using localStorage (needs actual categories and their codes)

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -5,7 +5,51 @@ if (typeof localStorage == 'undefined') {
 	}
 }
 
+function refreshCategoryList() {
+  if (typeof Categories == 'undefined') return;
+  var filterList  = $('#categoryFilters ul');
+  var html = [];
+  $.each(Categories, function(index, category) {
+    html.push('<li><input onchange="categoryFilterChanged($(this))" type="checkbox"');
+    if (category.active) html.push(' checked="checked"');
+    html.push(' data-id="')
+    html.push(category.id);
+    html.push('" />');
+    html.push(category.label);
+    html.push('</li>');
+  });
+  filterList.html(html.join(''));
+}
+
+function categoryFilterChanged(filterElem) {
+  var category = Categories[filterElem.data('id')];
+  category.active = $(filterElem).is(':checked')
+  storeUserCategories();
+}
+
+function check_all_categories() {
+  $.each(Categories, function(index, category) {
+    category.active = true;
+  });
+  refreshCategoryList();
+  storeUserCategories();
+}
+
+function clear_all_categories() {
+  $.each(Categories, function(index, category) {
+    category.active = false;
+  });
+  refreshCategoryList();
+  storeUserCategories();
+}
+
 $(document).ready(function() {
-	localStorage.setItem('test','sample');
-	if (localStore.getItem('test') == 'sample') $('article').html('Local storage works');
+	//localStorage.setItem('test','sample');
+	//if (localStorage.getItem('test') == 'sample') $('article').html('Local storage works');
+
+  // load and activate the category filters
+  $('.check_all_categories').click(function(e) { check_all_categories(); e.preventDefault(); });
+  $('.clear_all_categories').click(function(e) { clear_all_categories(); e.preventDefault(); });
+  refreshCategoryList();
 });
+
